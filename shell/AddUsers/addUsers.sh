c3b86fc9e7b05e39b74ce32c5e9cb605f5a4d030
jjkislele <jjkjjk333@sina.com>
2015-04-22 17:01:25 +0800

    first

--- shell/AddUsers/addUsers.sh
+++ shell/AddUsers/addUsers.sh
@@ -1,9 +1,9 @@
-#
-#
-# useradd
-# chpasswd
-# chown
-# chmod
-# edquota
-#
+#!/bin/sh
+number=1
+while [ $number -le 10 ]
+do 
+	useradd user$number 
+	echo A user was added. 
+	number = `expr $number + 1`
+done
 
